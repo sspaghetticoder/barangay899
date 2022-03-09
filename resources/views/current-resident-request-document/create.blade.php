9ae36a818e449f533feeb343dd4a2d79b1ccc396
Jade__ <jadelestersoriano@gmail.com>
2022-03-10 00:37:47 +0800

    home button

--- resources/views/current-resident-request-document/create.blade.php
+++ resources/views/current-resident-request-document/create.blade.php
@@ -4,7 +4,16 @@
     {{-- anonymous component: views -> components -> header blade --}}
     <x-header>
         <li class="nav-item">
-            <a class="nav-link ms-4" href="{{ route('home') }}">Home</a>
+            <a class="nav-link ms-4 d-none d-sm-block" href="{{ route('home') }}">Home</a>
+            <a class="nav-link ms-4 d-block d-sm-none" href="{{ route('home') }}">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-house"
+                    viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z" />
+                    <path fill-rule="evenodd"
+                        d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z" />
+                </svg>
+            </a>
         </li>
     </x-header>
 @endsection
@@ -249,7 +258,8 @@
                                     <a href="#" class="w-100 btn btn-primary form-btn btn-cancel">Cancel</a>
                                 </div>
                                 <div class="col-12 col-md-3 mt-2 mt-md-0 col-lg-4 ">
-                                    <input type="submit" class="w-100 btn btn-primary form-btn btn-next border-0" value=" Next ">
+                                    <input type="submit" class="w-100 btn btn-primary form-btn btn-next border-0"
+                                        value=" Next ">
                                 </div>
                             </div>
                         </div>
@@ -278,7 +288,8 @@
                 {{ \Session::get('Exception')['message'] ?? 'N/A' }}
             </x-slot>
 
-            <button type="button" class="btn btn-primary px-4 form-btn btn-next border-0" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary px-4 form-btn btn-next border-0"
+                data-bs-dismiss="modal">Close</button>
         </x-modal>
     @endif
 @endsection
