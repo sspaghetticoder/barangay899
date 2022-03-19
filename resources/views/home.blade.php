251a60316418f46357369ee546cd05c0f82d3a05
sspaghetticoder <jadefreelancedev@gmail.com>
2022-03-19 10:11:11 +0800

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,13 +11,11 @@
                             Welcome to Barrio Puso!
                         </h1>
                         <p class="page-header-text lead">
-                            {{-- A barangay that serve and help with a heart. --}}
-                            Lorem ipsum dolor sit amet consectetur adipisicing.
+                            A barangay that serve and help with a heart.
                         </p>
                         <p class="page-header-text text-sm">
-                            {{-- You can now request any documents from our barangay here and give feedback to us for the
-                            betterment of our barangay and service. --}}
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto corrupti explicabo mollitia quasi labore praesentium rem fuga.
+                            You can now request any documents from our barangay here and give feedback to us for the
+                            betterment of our barangay and service.
                         </p>
                     </div>
                     <div class="col-12 col-lg-6">
